8618e730a0be6fd18d1ff5b5f32b4889cc2c0577
ucoccto <edu1.ucoc@gmail.com>
2026-07-27 10:28:55 +0900

    Feat: 보안그룹 작성중

--- basic/main.tf
+++ basic/main.tf
@@ -12,3 +12,24 @@ data "aws_subnets" "default" {
       values = [data.aws_vpc.default.id]
     }
 }
+
+# 3. 보안그룹 생성 선언 - EC2 진입 하는데 인바운드 IP/포트, 아웃바운드 IP/포트 설정 => 접근 제한!!
+resource "aws_security_group" "DE-AI-25-IaC-TF-GROUP" {
+    # 메타 정보
+    name = "terraform-25-sg"
+    description = "de-ai-25 계정이 생성한 보안 그룹"
+    # 보안 그룹은 VPC에 종속되어서 구성됨
+    # id => 리소스명-해시값(중복x, 고유값)
+    vpc_id = data.aws_vpc.default.id
+    # 인바운드   (외부 트레픽이 내부로 들어옴) -> 일단 필요한 만큼 생성 -> 추후 반복문?등 문법효율적 활용을 통해 구성
+    ingress = {
+        
+    }
+    ingress = {
+
+    }
+    # 아웃바운드 (내부 트레픽이 외부로 나감)
+    egress = {
+
+    }
+}
