cbdcea87f5be29f4b584894fb75297cd49d9767e
ucoccto <edu1.ucoc@gmail.com>
2026-07-27 15:36:45 +0900

    Feat: count 예제 적용

---
+++ tf-step2/outputs.tf
@@ -0,0 +1,14 @@
+# Ec2 생성시 자동으로 부여되는 IP
+output "public_ip" {
+    # TODO: count 사용에 따라 [*] 조정
+    value = aws_instance.DE-AI-25-IaC-TF[*].public_ip
+}
+# Ec2 인스턴스 ID 출력
+output "instance_id" {
+    #value = aws_instance.DE-AI-25-IaC-TF.id
+    # TODO: count에 따라 출력 방식 변경
+    value = [
+        # aws_instance.DE-AI-25-IaC-TF 하나씩 꺼내서(instance.id) instance 담는다
+        for instance in aws_instance.DE-AI-25-IaC-TF : instance.id
+    ]
+}
