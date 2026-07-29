a837852803deddcc2d5c831d3098a4fe618f7277
ucoccto <edu1.ucoc@gmail.com>
2026-07-29 11:07:33 +0900

    Feat: 프라이빗 서브넷 전용 라우트 테이블 연결

--- tf-step4/vpc.tf
+++ tf-step4/vpc.tf
@@ -70,3 +70,9 @@ resource "aws_route_table" "private" {
     Name = "DE-AI-25-company-private-rt"
   }
 }
+
+# 최종연결 (프라이빗 서브넷 -> 프라이빗전용 라우트테이블)
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
