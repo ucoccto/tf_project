5c4185523e0d0bbe49373469d59cfc9aadd51149
ucoccto <edu1.ucoc@gmail.com>
2026-07-31 16:25:46 +0900

    Feat: encode 수정

--- tf-step5/launch_template.tf
+++ tf-step5/launch_template.tf
@@ -12,7 +12,7 @@ resource "aws_launch_template" "web" {
   # 보안 그룹 지정
   vpc_security_group_ids = [aws_security_group.web.id]
   # 서버 구성후 초기 작업 -> 쉘스크립트를 읽어서 => base64 인코딩처리 => 실행되게 구성
-  user_data = base64decode(templatefile("${path.module}/userdata-web.sh.tftpl", {
+  user_data = base64encode(templatefile("${path.module}/userdata-web.sh.tftpl", {
     # WEB -> proxy -> Internal ALB (이 리소스의 IP 혹은 dns 알아야 전달)
     # 해당 값을 가져가서 nginx conf 파일을 완성함
     internal_alb_dns = aws_lb.internal.dns_name
@@ -43,7 +43,7 @@ resource "aws_launch_template" "was" {
     name = aws_iam_instance_profile.ec2_ssm.name
   }
   vpc_security_group_ids = [aws_security_group.was.id]
-  user_data = base64decode(templatefile("${path.module}/userdata-was.sh.tftpl", {
+  user_data = base64encode(templatefile("${path.module}/userdata-was.sh.tftpl", {
     # rds 세팅값 설정
     db_host = aws_db_instance.mysql.address
     db_port = aws_db_instance.mysql.port
