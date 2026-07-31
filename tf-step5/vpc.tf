9b48c2df73c00355962fc27da74361eb8629010b
ucoccto <edu1.ucoc@gmail.com>
2026-07-31 13:07:59 +0900

    Feat: VPC-public subnet 구성

--- tf-step5/vpc.tf
+++ tf-step5/vpc.tf
@@ -17,6 +17,21 @@ resource "aws_internet_gateway" "main" {
 }
 
 # Public Subnets - Public ALB, NAT Gateway
+resource "aws_subnet" "public" {
+  # 반복데이터 세팅 (cidr)
+  for_each                  = local.public_subnets
+  vpc_id                    = aws_vpc.main.id
+  cidr_block                = each.value
+  # 키값이 a면 a에 맞는 값들로 구성, c도 동일함
+  availability_zone         = local.azs[each.key]
+  map_public_ip_on_launch   = true
+  # DE-AI-25-IaC-3tier-V1-PUBLIC-A, DE-AI-25-IaC-3tier-V1-PUBLIC-C
+  tags = {
+    Name = "${local.project}-PUBLIC-${upper(each.key)}"
+    # 커스텀 태그
+    Tier = "public"
+  }
+}
 
 # Private Application Subnets - Web, Was, internal ALB
 
