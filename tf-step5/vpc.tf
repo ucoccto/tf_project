8223caeea8bf6e9a8fb0dda339581c2f6fa610ff
ucoccto <edu1.ucoc@gmail.com>
2026-07-31 11:26:37 +0900

    Feat: vpc 기본 골격

---
+++ tf-step5/vpc.tf
@@ -0,0 +1,18 @@
+# VPC
+
+# Internet Gateway
+
+# Public Subnets - Public ALB, NAT Gateway
+
+# Private Application Subnets - Web, Was, internal ALB
+
+# Private Db Subnets - RDS
+
+# Public Route Table/association 
+
+# Nate Gateway - eip
+
+# Private App Route Table/association  - Web, Was
+
+# Private Db Route Table/association  - RDS
+
