b8d81fbf85fededb51b4c924b044379731279dee
ucoccto <edu1.ucoc@gmail.com>
2026-07-31 15:32:05 +0900

    Feat: 런치 템플릿 구성 1

--- tf-step5/launch_template.tf
+++ tf-step5/launch_template.tf
@@ -1,7 +1,10 @@
 ###################################
 # web ec2용 -> launch template 요소(ASG 내에서 사용)를 사용하여 생성됨
 ###################################
-
+resource "aws_launch_template" "web" {
+  name_prefix = "${local.project}-WEB-" # 증감이 수시로 발생해도 중복 x
+  image_id    = data.aws_ami.amazon_linux
+}
 
 ###################################
 # was ec2용 -> launch template 요소(ASG 내에서 사용)를 사용하여 생성됨
