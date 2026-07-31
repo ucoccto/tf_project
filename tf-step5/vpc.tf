0074b48af1f14776377f4e02e0a45459350899b9
ucoccto <edu1.ucoc@gmail.com>
2026-07-31 11:30:24 +0900

    Feat: vpc, igw 구성 완료

--- tf-step5/vpc.tf
+++ tf-step5/vpc.tf
@@ -1,6 +1,20 @@
 # VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "${local.project}-VPC"
+  }
+}
 
 # Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${local.project}-IGW"
+  }
+}
 
 # Public Subnets - Public ALB, NAT Gateway
 
