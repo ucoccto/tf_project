87c8ce7cb0b67b9227b283b19005d0f78fb6f232
ucoccto <edu1.ucoc@gmail.com>
2026-08-03 14:29:08 +0900

    Feat: 유효성 검사후 오류 조정, 포멧조정

--- tf-step6/infra/12_output.tf
+++ tf-step6/infra/12_output.tf
@@ -5,13 +5,13 @@ output "aws_region" {
   value = var.aws_region
 }
 output "cluster_name" {
-    # 임시 세팅
+  # 임시 세팅
   value = local.cluster_name
 }
 # ────────────────────────────────────────────────
 # VPC 설정 관련 변수
 # ────────────────────────────────────────────────
 output "public_subnets" {
-    # 임시 세팅
+  # 임시 세팅
   value = local.public_subnets
 }
