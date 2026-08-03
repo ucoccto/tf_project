981653a0d2bc5191317de530584a2d8efe37f0b6
ucoccto <edu1.ucoc@gmail.com>
2026-08-03 11:31:18 +0900

    Feat: VPC 관련 변수 설정

--- tf-step6/infra/02_variables.tf
+++ tf-step6/infra/02_variables.tf
@@ -17,6 +17,40 @@ variable "environment" {
   default     = "dev"
 }
 
+# ────────────────────────────────────────────────
+# VPC, 서브넷(2개), AZ, CIDR <- 네트워크 관련 변수
+# ────────────────────────────────────────────────
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+variable "availability_zones" {
+  description = "Multi-AZ 구성에 사용할 가용 영역 2개"
+  type        = list(string)
+  default     = ["ap-northeast-2a","ap-northeast-2c"] # 리전에 맞게 구성
+  # 유효성 검사 표기 (가용영역이 2개 이하이거나, c 존이 없는 경우)
+  validation {
+    condition = length(var.availability_zones) == 2
+    error_message = "본 구성은 정확하게 2개의 가용영역을 사용합니다."
+  }
+}
+variable "public_subnet_cidrs" {
+  description = "Public Subnet CIDR 목록"
+  type        = list(string)
+  default     = ["10.0.1.0/24","10.0.2.0/24"]
+}
+variable "app_subnet_cidrs" {
+  description = "EKS Auto Mode Node/Pod용 Private Subnet CIDR 목록"
+  type        = list(string)
+  default     = ["10.0.11.0/24","10.0.12.0/24"]
+}
+variable "db_subnet_cidrs" {
+  description = "RDS 전용 Private Subnet CIDR 목록"
+  type        = list(string)
+  default     = ["10.0.21.0/24","10.0.22.0/24"]
+}
+
 
 
 
