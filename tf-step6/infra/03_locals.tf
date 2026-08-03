0b0aa9fe30b071a5c2f8a65f840b8dad24b8c07d
ucoccto <edu1.ucoc@gmail.com>
2026-08-03 13:25:23 +0900

    Feat: 나머지 서브넷, 공통 태그 구성

--- tf-step6/infra/03_locals.tf
+++ tf-step6/infra/03_locals.tf
@@ -9,7 +9,7 @@ locals {
   public_subnets = {
     for index, key in local.az_keys : key => {
         az   = var.availability_zones[ index ]
-        cidr = var.var.public_subnet_cidrs[ index ]
+        cidr = var.public_subnet_cidrs[ index ]
     }
   }
   # 위의 구성으로 나오는 최종 결과
@@ -24,19 +24,25 @@ locals {
   #     }
   #   }
 
+  # 가용영역이 변경되거나 추가/감소 되거나, cidr 구성이 변경시 자동처리되게 재구성
   app_subnets = {
-    a = "10.0.11.0/24"
-    c = "10.0.12.0/24"
+    for index, key in local.az_keys : key => {
+        az   = var.availability_zones[ index ]
+        cidr = var.app_subnet_cidrs[ index ]
+    }
   }
   db_subnets = {
-    a = "10.0.21.0/24"
-    c = "10.0.22.0/24"
+    for index, key in local.az_keys : key => {
+        az   = var.availability_zones[ index ]
+        cidr = var.db_subnet_cidrs[ index ]
+    }
   }
 
-
+  # 모든 aws 리소스에 공통으로 적용
   common_tags = {
-    Project     = local.project
+    Project     = var.project_name
     Environment = var.environment
     ManageBy    = "Terraform"
+    Version     = "v2-eks-auto"
   }
 }
