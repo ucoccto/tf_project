2764b9cdbe30ae21279d17079250daa94b520214
ucoccto <edu1.ucoc@gmail.com>
2026-08-03 15:32:25 +0900

    Feat: EKS 클러스터 구성 1

--- tf-step6/infra/08_eks.tf
+++ tf-step6/infra/08_eks.tf
@@ -1,8 +1,20 @@
 # ────────────────────────────────────────────────
 # EKS Auto Mode 클러스터 생성 선언
 # ────────────────────────────────────────────────
+
+# 쿠버네티스 컨트로 플레인 생성, EKS Auto Mode 기능 활성화 
 resource "aws_eks_cluster" "main" {
-  
+    # eks 클러스터 이름
+    name = local.cluster_name
+
+    # 버전 (1.35)
+    version = var.kubernetes_version
+
+    # EKS Control plane이 AWS 리소스 관리할대 IAM role
+    role_arn = aws_iam_role.eks_cluster.arn
+
+    # Auto Mode(자동 직접 관리) 이므로 컨트롤 플레인에 필요한 기능등 별도로 addon 하지 않음
+    bootstrap_self_managed_addons = false
 }
 
 
