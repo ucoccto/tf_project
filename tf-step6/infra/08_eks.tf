96a9a4ab7df70cc8f3bce5a5a40ee862a5cb5e6a
ucoccto <edu1.ucoc@gmail.com>
2026-08-04 09:45:14 +0900

    Feat: 특정 역활/사용자에게 eks 클러스터 관리 권한을 부여하는 엔트리 작업

--- tf-step6/infra/08_eks.tf
+++ tf-step6/infra/08_eks.tf
@@ -123,11 +123,23 @@ resource "aws_eks_addon" "metrics_server" {
 
 
 # ────────────────────────────────────────────────
-# IAM Role 부분 추가 등록등 처리
+# IAM Role 부분 추가 등록등 처리 - 추가 관리자 access 엔트리
 # ────────────────────────────────────────────────
-# resource "aws_eks_access_entry" "admin" {
-  
-# }
+# var.addtional_admin_role_arns에 설정된 계정들도 EKS 클러스터에 접근하도록 관리 주체도 등록
+# 현재는 비워 있음 => []
+resource "aws_eks_access_entry" "admin" {
+  # 등록된 사용자 수 만큼 eks 클러스터 관리자에 등록하기 위해 엔트리 반복 데이터로 배치
+  for_each = var.addtional_admin_role_arns
+
+  # 대상 eks 클러스터
+  cluster_name = aws_eks_cluster.main.name
+
+  # 접근할 role arn
+  principal_arn = each.value
+
+  # 타입 : 일반 Iam 사용자, 다른 일반 role에 부여
+  type = "STANDARD"
+}
 # resource "aws_eks_access_policy_association" "admin" {
   
 # }
